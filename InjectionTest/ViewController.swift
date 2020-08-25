00107f9ef7e044146d297e7a4e5fbdb15c60afe3
Koichiro Eto <koichiro.eto@gmail.com>
2020-08-25 09:51:10 +0900

    Initial Commit

---
+++ InjectionTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InjectionTest
+//
+//  Created by Koichiro Eto on 2020/08/25.
+//  Copyright © 2020 Koichiro Eto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
